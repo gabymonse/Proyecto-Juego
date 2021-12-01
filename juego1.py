e8441856a6119c8e0af6c81b0ee7aa3acca226db
gabymonse <gabymonserrat97@gmail.com>
2021-12-01 15:59:54 -0300

    va mas o menos. le falta preguntar al principio y decir al final bajando o subiendo

--- juego1.py
+++ juego1.py
@@ -1,19 +1,40 @@
 pisos = [1, 2, 3]
-bajar = [pisos[0], pisos[1]]
-subir = [pisos[1], pisos[2]]
+bajar = [1, 2]
+subir = [2, 3]
+
 accion = ["bajar", "BAJAR", "subir", "SUBIR"]
 
-def piso(piso_actual):
- piso_actual = input("¿donde estas? ")
- if piso_actual in pisos:
-    bajar_o_subir = input("¿quieres bajar o subir? ")
-    if bajar_o_subir in accion:
-        a_donde = input("¿a que piso quieres ir? ")
-        if a_donde== pisos[0]:
-            return "solo puedes subir a ", subir
-        if a_donde == pisos[1]:
-              return "puedes subir a ", pisos[2], " o bajar a ", pisos[0]
-        if a_donde == pisos[2]:
-               return "puedes bajar a ", bajar
+#piso_actual = input("¿donde estas? ")
 
-print(piso(pisos))
+def piso(x):
+
+    
+    if x in pisos:
+        if x in subir:
+            input("¿a que piso quieres bajar? ")
+            
+          
+        if x in bajar:
+            input("¿a que piso quieres subir? ")
+            
+        if x in bajar and x in subir:
+            input ("¿quieres bajar o subir? ")
+            
+
+    #f x in alto:
+       # return "bajando..."
+    #if x in bajo:
+
+    
+
+      #if bajar_o_subir in accion:
+       #  a_donde = input("¿a que piso quieres ir? ")
+       #  if a_donde== pisos[0]:
+         #     return "solo puedes subir a ", subir
+        # if a_donde == pisos[1]:
+           #   return "puedes subir a ", pisos[2], " o bajar a ", pisos[0]
+         #if a_donde == pisos[2]:
+          #     return "puedes bajar a ", bajar
+
+print(piso(3))
+
